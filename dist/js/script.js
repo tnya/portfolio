3b99f2f837497983b3c7427ec497ea7b6f100f6a
Tanya Ramirez <tcramire@gmail.com>
2017-08-29 00:13:57 -0300

    detalles

--- dist/js/script.js
+++ dist/js/script.js
@@ -20253,11 +20253,20 @@ if (jQuery) {
   };
 })(jQuery);
 
-/* NAVBAR */
 $(document).ready(function(){
     $("#primary-navigation").click(function(){
         $("#secondary-navigation").toggleClass('opened');
         $("#main").toggleClass('slide-left');
         $("#main").toggleClass('animate');
     });
+
+
+    /*inicializacion portafolio*/
+
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+    /*fin portafolio*/
 });
